8379244039e67aee383251e6f787e286d1df37c7
Michael Goerz <goerz@stanford.edu>
2015-10-17 14:27:40 -0400

    Test of advanced SSH-key options

--- tests/test_sshkeys.py
+++ tests/test_sshkeys.py
@@ -171,6 +171,8 @@ def test_pubkey_line_without_comment():
     assert k.fingerprint == _bin_fp(
         'b9:e2:58:1a:74:fc:62:13:52:ad:f7:28:0b:09:91:54'
     )
+    assert k.options == OrderedDict()
+    assert k.comment is None
 
 
 def test_pubkey_line_with_options_and_comment():
@@ -198,3 +200,30 @@ def test_pubkey_line_with_options_and_comment():
 
     # reserializtion
     assert keyline == k.to_pubkey_line()
+
+
+def test_pubkey_line_with_quote_options():
+    keyline = (r'command="sh -c \"mysqldump db1 -u fred1 -p\"",environment="P'
+               r'ATH=/bin:/usr/bin/:/opt/gtm/bin",no-x11-forwarding '
+               'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDDN2n2kt99/aYpPbOZRJeGV'
+               'yFs74R1/QCqN351cuXLGK93lalDyIuIiUvMEYezregae1xDWXtCD+q9HMQpfl'
+               '62R2R1h3b8CMX8fpcGGXHJAxFWg/Sz8qXcbOeTRKdnBIWlUrkDi/7hWKZdXLs'
+               'iSPJeX9wmLhA5HCdHye1yFlGxSixTVK2fXyS9ZFEbBcIL8Aiq2EMQktCy2gDO'
+               'iJArpCF7pvsGqiLUxdCpOT+wuL+oGV47yVveGt9TcesnmZ1HxESXAIS22Vo2M'
+               'nTABxdNxNrs1ih3+4wdJ+gpoLo0lRNdjARRlcoH/fJvrXdbOrf//ARzuR9JKf'
+               'yKz+9aUEPxGtlEStbVysTjY2M3+Z4msbxh4x3ezpujhzpFCeLDHcAPg/HS6Go'
+               'O7zGcdJ8knCZK5ujOvFku03Es+jLrGNjACDOlLSYf9RHPqHvo/Fn+lCLJWZoc'
+               '0qiuICuHbEDU0fJ4qbVovZtdQtTwzQ8Az+VsLhJfehhadvb5hOCw3o4i9j1dJ'
+               'zcNfKJiBhab25GdfEYE097fDoYu/M0mi14AHWR0KI9o9Fd526x9B6c6gfljbH'
+               'JZcMGXhzfyO6nIsbZK6teJR7qh/8EQ7shOyfdcJkexvsbeNm12VTW34ar+Fjr'
+               'ApgN1QtY1+/6SDNSeOQqnBu2qENQVllSCfxOholMnVpO5ly1G2Q==')
+    k = Key.from_pubkey_line(keyline)
+    opts = OrderedDict([('command', 'sh -c "mysqldump db1 -u fred1 -p"'),
+                        ('environment', 'PATH=/bin:/usr/bin/:/opt/gtm/bin'),
+                        ('no-x11-forwarding', True)])
+    assert k.options == opts
+    assert k.comment is None
+
+    # reserializtion
+    assert keyline == k.to_pubkey_line()
+
