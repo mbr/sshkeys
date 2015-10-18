a3a15b7631b7e78f6a1b1e4353f9912a3b635b13
Michael Goerz <goerz@stanford.edu>
2015-10-18 10:48:07 -0400

    Add test for command option with backslashes

--- tests/test_sshkeys.py
+++ tests/test_sshkeys.py
@@ -227,3 +227,26 @@ def test_pubkey_line_with_quote_options():
     # reserializtion
     assert keyline == k.to_pubkey_line()
 
+
+def test_pubkey_line_with_backslash_command():
+    keyline = (r'command="echo \"\HELLO\"" '
+               'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDDN2n2kt99/aYpPbOZRJeGV'
+               'yFs74R1/QCqN351cuXLGK93lalDyIuIiUvMEYezregae1xDWXtCD+q9HMQpfl'
+               '62R2R1h3b8CMX8fpcGGXHJAxFWg/Sz8qXcbOeTRKdnBIWlUrkDi/7hWKZdXLs'
+               'iSPJeX9wmLhA5HCdHye1yFlGxSixTVK2fXyS9ZFEbBcIL8Aiq2EMQktCy2gDO'
+               'iJArpCF7pvsGqiLUxdCpOT+wuL+oGV47yVveGt9TcesnmZ1HxESXAIS22Vo2M'
+               'nTABxdNxNrs1ih3+4wdJ+gpoLo0lRNdjARRlcoH/fJvrXdbOrf//ARzuR9JKf'
+               'yKz+9aUEPxGtlEStbVysTjY2M3+Z4msbxh4x3ezpujhzpFCeLDHcAPg/HS6Go'
+               'O7zGcdJ8knCZK5ujOvFku03Es+jLrGNjACDOlLSYf9RHPqHvo/Fn+lCLJWZoc'
+               '0qiuICuHbEDU0fJ4qbVovZtdQtTwzQ8Az+VsLhJfehhadvb5hOCw3o4i9j1dJ'
+               'zcNfKJiBhab25GdfEYE097fDoYu/M0mi14AHWR0KI9o9Fd526x9B6c6gfljbH'
+               'JZcMGXhzfyO6nIsbZK6teJR7qh/8EQ7shOyfdcJkexvsbeNm12VTW34ar+Fjr'
+               'ApgN1QtY1+/6SDNSeOQqnBu2qENQVllSCfxOholMnVpO5ly1G2Q==')
+    k = Key.from_pubkey_line(keyline)
+    opts = OrderedDict([('command', 'echo "\HELLO"'), ])
+    assert k.options == opts
+    assert k.comment is None
+
+    # reserializtion
+    assert keyline == k.to_pubkey_line()
+
